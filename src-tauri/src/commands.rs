5d6f6fe879886dafae83766798da1148a656ebd3
myusuke <muzin00@gmail.com>
2025-08-10 16:51:37 +0900

    Add audio recording and playback functionality in App.vue; update start_recording command to return audio data in Rust backend.

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,9 +1,11 @@
-use std::{thread, time};
+use std::sync::mpsc;
 
 use cpal::{
     self,
     traits::{DeviceTrait, HostTrait, StreamTrait},
 };
+
+use serde::{Deserialize, Serialize};
 
 #[tauri::command]
 pub fn get_input_devices() -> Vec<String> {
@@ -15,8 +17,15 @@ pub fn get_input_devices() -> Vec<String> {
         .collect()
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AudioData {
+    channels: u16,
+    sample_rate: u32,
+    samples: Vec<f32>,
+}
+
 #[tauri::command]
-pub fn start_recording() -> Result<(), String> {
+pub fn start_recording() -> Result<AudioData, String> {
     let host = cpal::default_host();
 
     // デフォルトの入力デバイスを取得
@@ -25,21 +34,25 @@ pub fn start_recording() -> Result<(), String> {
         .ok_or("No default input device")?;
 
     // デバイスのデフォルト設定を取得
-    let config = device.default_input_config().map_err(|e| e.to_string())?;
+    let supported_config = device.default_input_config().map_err(|e| e.to_string())?;
 
-    println!("Recording with config: {:?}", config);
+    println!("Recording with supported config: {:?}", supported_config);
 
     // サンプルフォーマットがF32でない場合はエラー
-    if config.sample_format() != cpal::SampleFormat::F32 {
+    if supported_config.sample_format() != cpal::SampleFormat::F32 {
         return Err("Unsupported sample format".to_string());
     }
+
+    let config = supported_config.into();
+
+    let (tx, rx) = mpsc::channel();
 
     // ストリームを作成
     let stream = device
         .build_input_stream(
-            &config.into(),
-            move |data: &[f32], _| {
-                println!("Received data: {:?}", data);
+            &config,
+            move |data: &[f32], _info| {
+                tx.send(data.to_vec()).unwrap();
             },
             |err| eprintln!("Error: {:?}", err),
             None,
@@ -51,7 +64,27 @@ pub fn start_recording() -> Result<(), String> {
 
     println!("Recording started...");
 
-    thread::sleep(time::Duration::from_secs(10));
+    let mut samples = Vec::<f32>::new();
 
-    Ok(())
+    while let Ok(data) = rx.recv() {
+        samples.extend(data);
+
+        println!("Samples length: {}", samples.len());
+
+        if samples.len() > 960000 {
+            break;
+        };
+    }
+
+    Ok(AudioData {
+        channels: config.channels,
+        sample_rate: config.sample_rate.0,
+        samples,
+    })
 }
+
+// #[test]
+// fn test_start_recording() {
+//     let audio_data = start_recording().unwrap();
+//     println!("Audio data: {:?}", audio_data);
+// }
--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,45 @@
 import { invoke } from '@tauri-apps/api/core';
 import { onBeforeMount, ref } from 'vue';
 
+interface AudioData {
+    channels: number;
+    sample_rate: number;
+    samples: number[];
+}
+
 const inputDevices = ref<string[]>([]);
+const audioData = ref<AudioData | null>(null);
 
 onBeforeMount(async () => {
   inputDevices.value = await invoke('get_input_devices');
 });
+
+async function startRecording() {
+  const data = await invoke<AudioData>('start_recording');
+  console.log(data);
+  audioData.value = data;
+}
+
+async function playAudio() {
+  if (!audioData.value) return;
+  const audioContext = new AudioContext();
+  const audioBuffer = audioContext.createBuffer(
+    audioData.value.channels,
+    audioData.value.samples.length,
+    audioData.value.sample_rate
+  );
+  audioBuffer.copyToChannel(new Float32Array(audioData.value.samples), 0);
+  const source = audioContext.createBufferSource();
+  source.buffer = audioBuffer;
+  source.connect(audioContext.destination);
+  source.start();
+}
 </script>
 
 <template>
     <select>
         <option v-for="device in inputDevices" :value="device" :key="device">{{ device }}</option>
     </select>
+    <button @click="startRecording">録音</button>
+    <button @click="playAudio">再生</button>
 </template>
