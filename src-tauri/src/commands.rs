1da64895b3644b048dcdb2c86cbbcf48a0a077c9
myusuke <muzin00@gmail.com>
2025-08-10 14:06:29 +0900

    Add start_recording command to enable audio recording functionality and update invoke handler in lib.rs

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,6 +1,8 @@
+use std::{thread, time};
+
 use cpal::{
     self,
-    traits::{DeviceTrait, HostTrait},
+    traits::{DeviceTrait, HostTrait, StreamTrait},
 };
 
 #[tauri::command]
@@ -11,4 +13,45 @@ pub fn get_input_devices() -> Vec<String> {
         .into_iter()
         .map(|device| device.name().unwrap())
         .collect()
+}
+
+#[tauri::command]
+pub fn start_recording() -> Result<(), String> {
+    let host = cpal::default_host();
+
+    // デフォルトの入力デバイスを取得
+    let device = host
+        .default_input_device()
+        .ok_or("No default input device")?;
+
+    // デバイスのデフォルト設定を取得
+    let config = device.default_input_config().map_err(|e| e.to_string())?;
+
+    println!("Recording with config: {:?}", config);
+
+    // サンプルフォーマットがF32でない場合はエラー
+    if config.sample_format() != cpal::SampleFormat::F32 {
+        return Err("Unsupported sample format".to_string());
+    }
+
+    // ストリームを作成
+    let stream = device
+        .build_input_stream(
+            &config.into(),
+            move |data: &[f32], _| {
+                println!("Received data: {:?}", data);
+            },
+            |err| eprintln!("Error: {:?}", err),
+            None,
+        )
+        .map_err(|e| e.to_string())?;
+
+    // ストリームを開始
+    stream.play().map_err(|e| e.to_string())?;
+
+    println!("Recording started...");
+
+    thread::sleep(time::Duration::from_secs(10));
+
+    Ok(())
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,7 +3,10 @@ mod commands;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![commands::get_input_devices])
+        .invoke_handler(tauri::generate_handler![
+            commands::get_input_devices,
+            commands::start_recording
+        ])
         .setup(|app| {
             if cfg!(debug_assertions) {
                 app.handle().plugin(
