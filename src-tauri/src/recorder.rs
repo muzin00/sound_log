79ae56ec41463f0d5128b3bb678f1ee67e0a93b3
myusuke <muzin00@gmail.com>
2025-08-12 18:07:27 +0900

    Remove unsupported sample format check in Recorder implementation to simplify audio configuration handling.

--- src-tauri/src/recorder.rs
+++ src-tauri/src/recorder.rs
@@ -78,12 +78,6 @@ impl Recorder {
 
         // デバイスのデフォルト設定を取得
         let default_config = device.default_input_config().unwrap();
-        let sample_format = default_config.sample_format();
-
-        // サンプルフォーマットがF32でない場合はエラー
-        if sample_format != cpal::SampleFormat::F32 {
-            panic!("Unsupported sample format: {:?}", sample_format);
-        }
 
         let mut records = Vec::new();
         records.push(Record::new(receiver, device, default_config.config()));
